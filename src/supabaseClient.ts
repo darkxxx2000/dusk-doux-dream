8282bcbe4b267c0e5f72f407e64b7ad4f8be09f3
darkxxx2000 <silvana1002010@gmail.com>
2026-03-26 11:17:51 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+export const supabase = createClient(
+  'https://iymumvrvxudapuanwpct.supabase.co',
+  'sb_publishable_YIiuz6u9aHYb_dJhmzjw5w_gj0YEndQ'
+)
