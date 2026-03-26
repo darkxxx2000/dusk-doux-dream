d7a9db2e57d6d2717d973fbf9c8d5d7dfa3c7b5f
darkxxx2000 <silvana1002010@gmail.com>
2026-03-26 12:32:01 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { supabase } from "./supabaseClient";
 
-async function initAuth() {
-  await supabase.auth.getSession();
-}
-
-initAuth().then(() => {
-  createRoot(document.getElementById("root")!).render(<App />);
-});
+createRoot(document.getElementById("root")!).render(<App />);
